07bcf3793a0c049d94d232358ef6827c89856b1d
Lakshmi180 <152608571+Lakshmi180@users.noreply.github.com>
2024-03-04 16:24:33 +0530

    Inital Commit

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from "react";
+
+const initialValues = {
+  theme: "light",
+  setDarkMode: () => {},
+};
+
+export const ThemeContext = createContext(initialValues);
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const setDarkMode = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  const value = {
+    theme,
+    setDarkMode,
+  };
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  return context;
+};
