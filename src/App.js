bb1f5460d1f29e0ee36bdeb1614db78302e3de33
LipikaBohare <122879725+LipikaBohare@users.noreply.github.com>
2024-03-05 16:27:30 +0530

    lipika ui changes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js";
 // components
 import { Sidemenu } from "./components/common/sidemenu/Sidemenu";
+import Forms from "./components/common/design/Forms";
+import Card from "./components/common/design/Card";
+import Button from "./components/common/design/Button";
+
 
 function App() {
   return (
     <div className="App">
       <Sidemenu />
+      <Forms />
+    <Card/>
+    <Button/>
     </div>
   );
 }
