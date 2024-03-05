5e0b102714d3ca1683e770f2010811825985b579
Krishna Prasad Chintapalli <krishnaprasad.c@teksacademy.com>
2024-03-05 15:54:35 +0530

    lstest

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js";
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js";
+
 // components
 import { Sidemenu } from "./components/common/sidemenu/Sidemenu";
 import { Route, Routes } from "react-router-dom";
