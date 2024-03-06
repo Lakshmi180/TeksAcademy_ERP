163cb0a4cb304affb678d4e79bd32de2c30201c3
Lakshmi180 <152608571+Lakshmi180@users.noreply.github.com>
2024-03-06 10:19:55 +0530

    package and app and card changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from "react";
 import "./App.css";
+
 // cdn
 import "bootstrap/dist/css/bootstrap.min.css";
 import "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js";
@@ -6,42 +8,32 @@ import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.mi
 // components
 import { Sidemenu } from "./components/common/sidemenu/Sidemenu";
 import Table from "./components/common/design/Table";
-import Card from "./components/common/design/Card";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import ReqireAuth from "./components/Layout/ReqireAuth";
+import PublicAuth from "./components/Layout/PublicAuth";
+import { Login } from "./components/Auth/Login";
+
 import Forms from "./components/common/design/Forms";
 import Card from "./components/common/design/Card";
 import Button from "./components/common/design/Button";
 
-import { Route, Routes } from "react-router-dom";
-import ReqireAuth from "./components/Layout/ReqireAuth";
-import PublicAuth from "./components/Layout/PublicAuth";
-
-import { Login } from "./components/Auth/Login";
-
 function App() {
   return (
-    <div className="App">
-      <Sidemenu />
-     <Table />
-     <Card />
-      <Forms />
-    <Card/>
-    <Button/>
-      {/* <Sidemenu /> */}
-      <Login />
-
-      {/* <Routes>
-      <Route element={<ReqireAuth/>}>
-
-     
-      </Route>
-      <Route element={<PublicAuth/>}>
-
-
-      </Route>
-
-    </Routes> */}
-
-      {/* <Login /> */}
+    <div className="app">
+      <div className="content-wrapper">
+        <Sidemenu />
+        <Router>
+          <Routes>
+            <Route path="/card" element={<Card />} />
+            {/* <Route path="/" exact element={<App />} />
+            <Route path="/table" element={<Table />} />
+           
+            <Route path="/button" element={<Button />} />
+            <Route path="/forms" element={<Forms />} /> */}
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
