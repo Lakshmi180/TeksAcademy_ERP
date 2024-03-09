8037b0b10d5d7bf9856a9396e86a08c3a99b24c2
Lakshmi180 <152608571+Lakshmi180@users.noreply.github.com>
2024-03-09 10:51:56 +0530

    sidebar changes

--- src/App.js
+++ src/App.js
@@ -1,63 +1,40 @@
-import React from "react";
+// import React from "react";
 import "./App.css";
-
-// cdn
-import "bootstrap/dist/css/bootstrap.min.css";
-import "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js";
-import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js";
-
 // components
 import { Sidemenu } from "./components/common/sidemenu/Sidemenu";
 import Table from "./components/common/design/Table";
-import { BrowserRouter as Router } from "react-router-dom";
 import { Login } from "./components/Auth/Login";
-
 import Forms from "./components/common/design/Forms";
 import Card from "./components/common/design/Card";
 import Button from "./components/common/design/Button";
-
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Outlet, BrowserRouter } from "react-router-dom";
 import ReqireAuth from "./components/Layout/ReqireAuth";
 import PublicAuth from "./components/Layout/PublicAuth";
 import Dashboard from "./components/pages/dashboard/Dashboard";
 import PublicLayout from "./components/Layout/PublicLayout";
+import { Topbar } from "./components/common/topbar/Topbar";
+import React, { useContext, useState } from "react";
 
 function App() {
+   const [isExpanded, setIsExpanded] = useState(true);
+
+   const toggleSidebar = () => {
+     setIsExpanded(!isExpanded);
+   };
   return (
-    <div className="App">
-      <Sidemenu />
-     <Table />
-     <Card />
-      <Forms />
-    <Card/>
-    <Button/>
-      {/* <Sidemenu /> */}
-      <Login />
-
-      {/* <Routes>
-      <Route element={<ReqireAuth/>}>
-        <Route path="/" element={<Dashboard/>} />
-      </Route>
-
-
-      <Route element={<PublicLayout/>}>
-        <Route path="/login" element={<Login/>}/>
-      </Route>
-
-    </Routes>
-
-
-
-     
-      </Route>
-      <Route element={<PublicAuth/>}>
-
-
-      </Route>
-
-    </Routes> */}
-
-      {/* <Login /> */}
+    <div className="app">
+      <Sidemenu
+        isExpanded={isExpanded}
+        setIsExpanded={setIsExpanded}
+        toggleSidebar={toggleSidebar}
+      />
+      <main className="">
+        <Topbar
+          isExpanded={isExpanded}
+          setIsExpanded={setIsExpanded}
+          toggleSidebar={toggleSidebar}
+        />
+      </main>
     </div>
   );
 }
