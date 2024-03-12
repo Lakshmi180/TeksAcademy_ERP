584dead5c331fddd7e3e2afaf742fa2d143c0bca
Lakshmi180 <152608571+Lakshmi180@users.noreply.github.com>
2024-03-12 18:02:43 +0530

    enrolled student  path added in sidebar

--- src/App.js
+++ src/App.js
@@ -74,6 +74,7 @@ function App() {
             <Route path="/settingstabs" element={<SettingsTabs />} />
             <Route path="roles" element={<Roles />} />
             <Route path="/createrole" element={<CreateRole />} />
+            <Route path="/studentdata" element={<Studentdata/>}/>
           </Routes>
         </div>
       </main>
